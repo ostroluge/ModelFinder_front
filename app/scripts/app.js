b89c1d28608cf05c5e8d57e0a7baca7c82b92fb7
Julien Flamen <flamen.julien@gmail.com>
2016-03-11 18:06:52 +0100

    Génération d'un projet AngularJS avec Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name modelFinderApp
+ * @description
+ * # modelFinderApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('modelFinderApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modelFinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the modelFinderApp
+ */
+angular.module('modelFinderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
