cf145d3b3b17e0caf2b8e6b6c59f17759828de43
FlorianeGb <fl.goubel@gmail.com>
2016-03-22 00:42:06 +0100

    Base structure

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,16 +8,26 @@
  *
  * Main module of the application.
  */
-var modelFinderApp = angular
-  .module('modelFinderApp', ['ngRoute']);
+
+var modelFinderApp = angular.module('modelFinderApp', ['ngRoute']);
 
 modelFinderApp.config(['$routeProvider',
   function($routeProvider) {
 
-    $routeProvider
-      .when('/main', {
+   
+    $routeProvider.
+    when('/main', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      });
+      }).
+    when('/student', {
+        controller:'StudentCtrl', 
+        templateUrl:'views/student.html', 
+    }).
+    when('/annonce', {
+        controller:'AnnonceCtrl', 
+        templateUrl:'views/annonce.html', 
+    }).
+    otherwise({redirectTo:'/'});  
   }
 ]);
