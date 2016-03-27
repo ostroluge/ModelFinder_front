525c4bc79aec2e537f17d50af5d96589da588740
Thomas <ostrowski.thomas@gmail.com>
2016-03-27 17:05:03 +0200

    format annonce data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,25 +14,25 @@ var modelFinderApp = angular.module('modelFinderApp', ['ngRoute']);
 modelFinderApp.config(['$routeProvider',
   function($routeProvider) {
 
-   
+
     $routeProvider.
     when('/main', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       }).
     when('/student', {
-        controller:'StudentCtrl', 
-        templateUrl:'views/student.html', 
+        controller:'StudentCtrl',
+        templateUrl:'views/student.html',
     }).
-    when('/annonce', {
-        controller:'AnnonceCtrl', 
-        templateUrl:'views/annonce.html', 
+    when('/annonces', {
+        controller:'AnnonceCtrl',
+        templateUrl:'views/annonce.html',
     }).
     when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
       }).
-    otherwise({redirectTo:'/'});  
-     
+    otherwise({redirectTo:'/'});
+
   }
 ]);
