68807953a3643eb5ce7124df8a27f7ef3383bb6b
Julien Flamen <flamen.julien@gmail.com>
2016-03-31 22:03:49 +0200

    Ajout des énumérations

--- app/scripts/controllers/annonce.js
+++ app/scripts/controllers/annonce.js
@@ -29,41 +29,79 @@ modelFinderApp.controller('AnnonceCtrl', function ($scope, $http, $location) {
   $scope.getIndex = function (annonce) {
     return $scope.message.indexOf(annonce);
   };
+
+  $scope.createAnnonce = function () {
+
+    var postObject = new Object();
+    postObject.title = $scope.titleAnnonce;
+    postObject.themeService = $scope.themeAnnonce;
+    postObject.categoryService = $scope.categoryAnnonce;
+    postObject.dateBegin = $scope.dateBeginAnnonce;
+    postObject.dateEnd = $scope.dateEndAnnonce;
+    postObject.accessories = $scope.accessoriesAnnonce;
+    postObject.hairColor = $scope.hairColorAnnonce;
+    postObject.lengthHair = $scope.lengthHairAnnonce;
+    postObject.skinTone = $scope.skinToneAnnonce;
+    postObject.heightMin = $scope.heightMinAnnonce;
+    postObject.heightMax = $scope.heightMaxAnnonce;
+    postObject.eyeColor = $scope.eyeColorAnnonce;
+
+    $http({
+      url: "http://localhost:8080/createAnnonce",
+      method: "POST",
+      dataType: "json",
+      data: postObject,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).success(function successCallback(response) {
+        if (response.response == "success") {
+          $scope.messageCreation = "Annonce créée"
+        } else {
+          $scope.messageCreation = "Erreur de création"
+        }
+      })
+      .error(function errorCallback(response) {
+        $scope.messageCreation = "Error " + response
+      });
+  };
+
+  $scope.getCategoriesEnum = function() {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:8080/getCategoriesEnum',
+    }).success(function(categories){
+      $scope.categoriesEnum = categories;
+    })
+  };
+
+  $scope.getEyeColorEnum = function() {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:8080/getEyeColorEnum',
+    }).success(function(eyeColor){
+      $scope.eyeColorEnum = eyeColor;
+    })
+  };
+
+  $scope.getLengthHairEnum = function() {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:8080/getLengthHairEnum',
+    }).success(function(length){
+      $scope.lengthHairEnum = length;
+    })
+  };
+
+  $scope.getSkinToneEnum = function() {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:8080/getSkinToneEnum',
+    }).success(function(skinTone){
+      $scope.skinToneEnum = skinTone;
+    })
+  };
+
 });
 
-$scope.createAnnonce = function () {
-
-  var postObject = new Object();
-  postObject.title = $scope.titleAnnonce;
-  postObject.themeService = $scope.themeAnnonce;
-  postObject.categoryService = $scope.categoryAnnonce;
-  postObject.dateBegin = $scope.dateBeginAnnonce;
-  postObject.dateEnd = $scope.dateEndAnnonce;
-  postObject.accessories = $scope.accessoriesAnnonce;
-  postObject.hairColor = $scope.hairColorAnnonce;
-  postObject.lengthHair = $scope.lengthHairAnnonce;
-  postObject.skinTone = $scope.skinToneAnnonce;
-  postObject.heightMin = $scope.heightMinAnnonce;
-  postObject.heightMax = $scope.heightMaxAnnonce;
-  postObject.eyeColor = $scope.eyeColorAnnonce;
-
-  $http({
-    url: "http://localhost:8080/createAnnonce",
-    method: "POST",
-    dataType: "json",
-    data: postObject,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).success(function successCallback(response) {
-      if (response.response == "success") {
-        $scope.messageCreation = "Annonce créée"
-      } else {
-        $scope.messageCreation = "Erreur de création"
-      }
-    })
-    .error(function errorCallback(response) {
-      $scope.messageCreation = "Error " + response
-    });
-};
 
