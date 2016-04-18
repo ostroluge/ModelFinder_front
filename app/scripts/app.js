0493f269cbf1a9d1fb7ba0989c8f675d135d3f94
Thomas <ostrowski.thomas@gmail.com>
2016-04-18 23:53:55 +0200

    add apply view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,47 +12,41 @@
 var modelFinderApp = angular.module('modelFinderApp', ['ngRoute']);
 
 modelFinderApp.config(['$routeProvider',
-  function($routeProvider) {
+  function ($routeProvider) {
 
 
-    $routeProvider.
-    when('/main', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      }).
-    when('/student', {
-        controller:'StudentCtrl',
-        templateUrl:'views/student.html',
-    }).
-    when('/annonces', {
-        controller:'AnnonceCtrl',
-        templateUrl:'views/list_annonces.html',
-    }).
-    when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-    }).
-    when('/detailAnnonce/:id_annonce', {
-        templateUrl: 'views/detail_annonce.html',
-        controller: 'DetailAnnonceCtrl'
-    }).
-      when('/updateAnnonce/:idAnnonce',{
+    $routeProvider.when('/main', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    }).when('/student', {
+      controller: 'StudentCtrl',
+      templateUrl: 'views/student.html',
+    }).when('/annonces', {
+      controller: 'AnnonceCtrl',
+      templateUrl: 'views/list_annonces.html',
+    }).when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    }).when('/detailAnnonce/:id_annonce', {
+      templateUrl: 'views/detail_annonce.html',
+      controller: 'DetailAnnonceCtrl'
+    }).when('/updateAnnonce/:idAnnonce', {
       controller: 'ModifyAnnonceCtrl',
-      templateUrl:'views/modify_annonce.html'
+      templateUrl: 'views/modify_annonce.html'
+    }).when('/createAnnonce', {
+      controller: 'AnnonceCtrl',
+      templateUrl: 'views/create_annonce.html'
+    }).when('/suggestionModel/:carnation_peau/:couleur_cheveux/:couleur_yeux/:longueur_cheveux/:taille_min/:taille_max', {
+      controller: 'SuggestionModelCtrl',
+      templateUrl: 'views/suggestion_modele.html'
+    }).when('/detailModel/:id_model', {
+      controller: 'DetailModelCtrl',
+      templateUrl: 'views/detail_model.html'
+    }).when('/apply/:id_annonce', {
+      controller: 'ApplyCtrl',
+      templateUrl: 'views/apply.html'
     }).
-    when('/createAnnonce', {
-      controller:'AnnonceCtrl',
-      templateUrl:'views/create_annonce.html'
-    }).
-     when('/suggestionModel/:carnation_peau/:couleur_cheveux/:couleur_yeux/:longueur_cheveux/:taille_min/:taille_max', {
-      controller:'SuggestionModelCtrl',
-      templateUrl:'views/suggestion_modele.html'
-    }).
-   when('/detailModel/:id_model', {
-      controller:'DetailModelCtrl',
-      templateUrl:'views/detail_model.html'
-    }).
-    otherwise({redirectTo:'/'});
+    otherwise({redirectTo: '/'});
 
   }
 ]);
